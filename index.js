18098b95b6dc990cafdc5bac2b1114b3524198f4
Brett Logan <lindluni@github.com>
2022-02-25 10:02:57 -0500

    Init

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core')
+
+const {Octokit} = require("@octokit/rest")
+const {retry} = require("@octokit/plugin-retry");
+const {throttling} = require("@octokit/plugin-throttling");
+const _Octokit = Octokit.plugin(retry, throttling)
+
+const body = core.getInput('BODY', {required: true, trimWhitespace: true}).split(' ')
+const org = core.getInput('ORG', {required: true, trimWhitespace: true})
+const repo = core.getInput('REPO', {required: true, trimWhitespace: true})
+const issueNumber = core.getInput('ISSUE_NUMBER', {required: true, trimWhitespace: true})
+const token = core.getInput('TOKEN', {required: true, trimWhitespace: true})
+
+const client = new _Octokit({
+    auth: token,
+    throttle: {
+        onRateLimit: (retryAfter, options, octokit) => {
+            octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+            if (options.request.retryCount === 0) {
+                octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+                return true;
+            }
+        },
+        onAbuseLimit: (retryAfter, options, octokit) => {
+            octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
+        },
+    }
+
+});
+
+(async function() {
+    const username = body[body.length - 1]
+    console.log(username)
+})()
